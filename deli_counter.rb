de4ac3e5f843e62b2b45dc0a4f36766b58da21bd
James Pier <jpier2012@gmail.com>
2019-04-03 14:44:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,11 @@
 =begin
 #line
   - show everyone their current place in line
-  - if nil, puts "This line is currently empty"
+  - if nil, puts "This line is currently empty."
 
 #take_a_number list, name
   - puts name + position in line
 
 #now_serving
+  - puts the next person in line and remove them from the front
+  - if nil puts "There is nobody waiting to be served!"
