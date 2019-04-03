8f312b9aec4a68ee83dee2eef771122b7b1df302
James Pier <jpier2012@gmail.com>
2019-04-03 14:44:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+=begin
+#line
+  - show everyone their current place in line
+  - if nil, puts "This line is currently empty"
+
+#take_a_number list, name
+  - puts name + position in line
+
+#now_serving
